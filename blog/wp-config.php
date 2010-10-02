b02b1df49ffaba7ff63ccafc55e050a897d2b22c
Jordan McKible <jmckible@mckible.(none)>
2010-10-02 19:17:05 +0000

    wp-config

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'mck');
+define('DB_NAME', 'mckible_wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'mckible');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'kl2QNJTE');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
